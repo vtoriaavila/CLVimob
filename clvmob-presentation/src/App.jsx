3fef7733743683a159cb01841b9a6eb8829ffffd
VITORIA AVILA BARROS DA CRUZ <20222SI0017@ifma.edu>
2024-08-12 18:21:32 -0300

    tipo ususario

--- clvmob-presentation/src/App.jsx
+++ clvmob-presentation/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './components/Home';
 import Login from './components/Login'; 
 import Cadastro from "./components/Cadastro";
+import TipoUsuario from './components/TipoUsuario';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/tipo-usuario" element={<TipoUsuario/>} />
       </Routes>
     </Router>
   );
---
+++ clvmob-presentation/src/components/TipoUsuario.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './TipoUsuario.css';
+import casa from './casa.jpg'; 
+import logo_branco from './logo_branco.png';  
+
+const TipoUsuario = () => {
+  const navigate = useNavigate();
+
+  const handleAdministradorClick = () => {
+    navigate('/cadastro-administrador');
+  };
+
+  const handleProprietarioClick = () => {
+    navigate('/cadastro-proprietario');
+  };
+
+  const handleLocatarioClick = () => {
+    navigate('/cadastro-locatario');
+  };
+
+  return (
+    <div className="tipoUsuarioContainer">
+      <div className="leftPanel">
+        <img src={logo_branco} alt="CLVimob Logo" className="logo" />
+        <h1>Cadastro</h1>
+        <p>Escolha o tipo de usuário</p>
+        <button className="usuarioButton" onClick={handleAdministradorClick}>Administrador</button>
+        <button className="usuarioButton" onClick={handleProprietarioClick}>Proprietário</button>
+        <button className="usuarioButton" onClick={handleLocatarioClick}>Locatário</button>
+      </div>
+      <div className="rightPanel">
+        <img src={casa} alt="Foto da casa" className="foto-casa" />
+      </div>
+    </div>
+  );
+}
+
+export default TipoUsuario;
